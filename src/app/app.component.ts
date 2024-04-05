2b7b2f21ade0325c645995bbb96bfcc5de7dafe3
Simon Jose Gil Romero <sgil@ADADOM.LOCAL>
2024-04-05 15:15:43 -0500

    change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,27 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './shared/components/nav-bar/nav-bar.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavBarComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, NavBarComponent, FooterComponent, ButtonModule, InputTextModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'AngularArquitectura';
+
+  nombre: string = '';
+
+  onSubmit() {
+    console.log('Nombre enviado: ' + this.nombre);
+    alert('Nombre enviado: ' + this.nombre);
+    this.nombre = ''; // Limpiar el campo después del envío
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,11 +5,15 @@ import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
 
+import { provideAnimations } from '@angular/platform-browser/animations';
+
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(
       withInterceptors([authInterceptor])
-    )
+    ),
+    provideAnimations()
   ]
 };
