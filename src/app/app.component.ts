d8ba9b266a231868363630ba3a4eb312f48a01c9
Simon Jose Gil Romero <sgil@ADADOM.LOCAL>
2024-04-12 15:26:55 -0500

    barras

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,15 @@ import { FooterComponent } from './shared/components/footer/footer.component';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { FormsModule } from '@angular/forms';
-
+import { ToolbarModule } from 'primeng/toolbar';
+import { MenuItem } from 'primeng/api';
+import { SplitButtonModule } from 'primeng/splitbutton';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavBarComponent, FooterComponent, ButtonModule, InputTextModule, FormsModule],
+  imports: [CommonModule, RouterOutlet, NavBarComponent, FooterComponent, ButtonModule, InputTextModule, FormsModule, ToolbarModule, SplitButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -20,16 +22,35 @@ export class AppComponent implements OnInit{
   title = 'AngularArquitectura';
 
   nombre: string = '';
+  items: MenuItem[] | undefined;
+
 
   ngOnInit(): void {
-    const urlDelIframe = window.location.href;
-    console.log('HOLA MUESTRO LA URL ', urlDelIframe);
-    window.parent.postMessage({ url: urlDelIframe }, '*');
+    this.sendUrl();
+    this.items = [
+      {
+          label: 'Update',
+          icon: 'pi pi-refresh'
+      },
+      {
+          label: 'Delete',
+          icon: 'pi pi-times'
+      }
+  ];
   }
+
+
 
   onSubmit() {
     console.log('Nombre enviado: ' + this.nombre);
     alert('Nombre enviado: ' + this.nombre);
-    this.nombre = ''; // Limpiar el campo después del envío
+    this.nombre = ''; 
   }
+
+  sendUrl () {
+    const urlDelIframe = window.location.href;
+    console.log('URL ENVIADA ', urlDelIframe);
+    window.parent.postMessage({ url: urlDelIframe }, '*');
+  }
+
 }
--- src/app/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -1,13 +1,78 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ToolbarModule } from 'primeng/toolbar';
+import { MenuItem } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
+import { SplitButtonModule } from 'primeng/splitbutton';
+import { DockModule } from 'primeng/dock';
+import { RadioButtonModule} from 'primeng/radiobutton';
+
+
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ToolbarModule, ButtonModule, InputTextModule, FormsModule, SplitButtonModule, DockModule, RadioButtonModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit {
+  items = [
+    {
+        label: 'Update',
+        icon: 'pi pi-refresh'
+    },
+    {
+        label: 'Delete',
+        icon: 'pi pi-times'
+    }
+];
 
+  dockItems = [
+    {
+        label: 'Finder',
+        icon: 'https://primefaces.org/cdn/primeng/images/dock/finder.svg'
+    },
+    {
+        label: 'App Store',
+        icon: 'https://primefaces.org/cdn/primeng/images/dock/appstore.svg'
+    },
+    {
+        label: 'Photos',
+        icon: 'https://primefaces.org/cdn/primeng/images/dock/photos.svg'
+    },
+    {
+        label: 'Trash',
+        icon: 'https://primefaces.org/cdn/primeng/images/dock/trash.png'
+    }
+];
+
+  position: 'bottom' | 'top' | 'left' | 'right' = 'bottom';
+  label: string = 'Select an option';
+
+  positionOptions = [
+    {
+        label: 'Bottom',
+        value: 'bottom'
+    },
+    {
+        label: 'Top',
+        value: 'top'
+    },
+    {
+        label: 'Left',
+        value: 'left'
+    },
+    {
+        label: 'Right',
+        value: 'right'
+    }
+];
+
+
+  ngOnInit() {
+
+  }
 }
