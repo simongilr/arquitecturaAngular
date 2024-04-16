6468965a5e4f42016af112624b051cb446833c78
Simon Jose Gil Romero <sgil@ADADOM.LOCAL>
2024-04-16 07:39:49 -0500

    change

--- src/app/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -8,27 +8,32 @@ import { FormsModule } from '@angular/forms';
 import { SplitButtonModule } from 'primeng/splitbutton';
 import { DockModule } from 'primeng/dock';
 import { RadioButtonModule} from 'primeng/radiobutton';
-
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [RouterLink, ToolbarModule, ButtonModule, InputTextModule, FormsModule, SplitButtonModule, DockModule, RadioButtonModule],
+  imports: [RouterLink, ToolbarModule, ButtonModule, InputTextModule, FormsModule, SplitButtonModule, DockModule, RadioButtonModule, CommonModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent implements OnInit {
-  items = [
-    {
-        label: 'Update',
-        icon: 'pi pi-refresh'
-    },
-    {
-        label: 'Delete',
-        icon: 'pi pi-times'
-    }
-];
+
+    buttons = [
+        { icon: 'pi pi-home', label: '', routerLink: ['/articles'], slot: 'start'},
+        { icon: 'pi pi-info-circle', label: '', routerLink: ['/articles', 'detalle'], slot: 'start' },
+        { icon: 'pi pi-folder', label: '', routerLink: ['/dashboard'], slot: 'start' },
+        { icon: 'pi pi-calendar', label: '', routerLink: ['/dashboard', 'calendar'], slot: 'start' },
+        { icon: 'pi pi-save', label: '', routerLink: ['/dashboard', 'calendar'], slot: 'start' },
+        { icon: 'pi-arrow-circle-down', label: '', routerLink: ['/register'], slot: 'end' },
+        { icon: 'pi pi-sign-in', label: '', routerLink: ['/login'], slot: 'end' },
+        { icon: 'pi pi-file-excel', label: '', routerLink: ['/register'], slot: 'end' },
+        { icon: 'pi pi-file-export', label: '', routerLink: '', slot: 'end' },
+        { icon: 'pi pi-sign-in', label: '', routerLink: ['/login'], slot: 'end' }
+
+    ];
 
   dockItems = [
     {
